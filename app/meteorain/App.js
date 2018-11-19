6175f6938700f7bbbc46c98b92a56a36aa18c21d
JulienDERONGS <julien.derongs@masagroup.net>
2018-11-19 18:55:15 +0100

    * Icons working     * Make master up to date again

--- app/meteorain/App.js
+++ app/meteorain/App.js
@@ -1,9 +1,21 @@
 import React from 'react';
-import { createStackNavigator } from 'react-navigation';
 import { createMaterialBottomTabNavigator } from 'react-navigation-material-bottom-tabs';
 import { Text, View, Button } from 'react-native';
 import { styles } from './src/styles/styles';
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons'
 
+const activeTintLabelColor = '#005555';
+const inactiveTintLabelColor = '#808080';
+
+class UploadScreen extends React.Component {
+  render() {
+    return (
+      <View style={styles.upload}>
+        <Text>Upload Screen</Text>
+      </View>
+    );
+  }
+}
 
 class GraphDayScreen extends React.Component {
   render() {
@@ -61,20 +73,16 @@ class GraphRoseScreen extends React.Component {
   }
 }
 
-class UploadScreen extends React.Component {
-  render() {
-    return (
-      <View style={styles.upload}>
-        <Text>Upload Screen</Text>
-      </View>
-    );
-  }
-}
-
 const BottomNav = createMaterialBottomTabNavigator(
   {
     Upload: {
       screen: UploadScreen,
+      navigationOptions: {
+        tabBarLabel: <Text style={{ fontSize: 10, color: activeTintLabelColor }}> Uploaded files </Text>,
+        tabBarIcon: ({ focused }) => (
+          <MaterialCommunityIcons name='format-list-bulleted' color={focused ? activeTintLabelColor : inactiveTintLabelColor} size={24} />
+        )
+      }
     },
     Day: {
       screen: GraphDayScreen,
@@ -90,10 +98,26 @@ const BottomNav = createMaterialBottomTabNavigator(
     },
   },
   {
+    shifting: true,
     initialRouteName: 'Upload',
-    activeColor: '#f0edf6',
-    inactiveColor: '#3e2465',
+    //activeColor: '#f0edf6',
+    //inactiveColor: '#3e2465',
+    activeTintColor: '#ffffff',
+    inactiveTintColor: '#2f2f2f2f',
     barStyle: { backgroundColor: '#694fad' },
+
+    /*navigationOptions: ({ navigation }) => ({
+      tabBarIcon: ({ focused, horizontal, tintColor }) => {
+        const { routeName } = navigation.state;
+        let iconName;
+        if (routeName === 'Upload') {
+          iconName = `add-circle-outline'${focused ? '' : '-outline'}`;
+        } else if (routeName === 'Settings') {
+          iconName = `ios-options${focused ? '' : '-outline'}`;
+        }
+        return <Ionicons name={iconName} size={horizontal ? 20 : 25} color={tintColor} />;
+      },
+    }),*/
   }
 );
 
